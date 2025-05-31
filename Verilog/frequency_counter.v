4d1b195d9c682d3be347d434e9d292228721aef7
radityankn <raditya.ep.rep@gmail.com>
2025-05-31 13:51:34 +0700

    updated counter module

--- Verilog/frequency_counter.v
+++ Verilog/frequency_counter.v
@@ -44,124 +44,12 @@ module frequency_counter(
     output reg [1:0] status
     );
 	
-	reg [31:0] measurement_count_internal;       //front-end measurement counter register, where counting happens and data stored before being pushed to the bus-facing register
+	
     reg [31:0] measurement_count_reg;            //bus-facing coarse counter data register, accessible through the wishbone bus
-    reg [4:0] phase_count_internal;             //front-end measurement counter register, where counting happens and data stored before being pushed to the bus-facing register
-    reg [9:0] phase_count_intermediate;
     reg [31:0] phase_count_reg;                  //bus-facing fine counter data register, accessible through the wishbone bus
     reg [7:0] counter_control_reg;               //counter control register, used to start the measurement and indicate whether a measurement has been finished or not
-    reg [15:0] measurement_state_machine;        //state machine to indicate whether input signal rising edge is present or not
-    reg measurement_begin;                       //state of the measurement state machine from the last clock cycle
-    reg measurement_is_done;                     //register of whether a measurement has been done or not, useful when waiting for rising edge
-    reg counter_ready;
-    reg interpolation_in_beginning_done;
-    reg [31:0] counter_pll_1;
 
     wire counter_reset_internal;                 //used to reset front-end counter register after each measurement
-
-    //this is the input signal edge detection. used for gating the reference clock
-    //so it doesn't increment when the signal is not present
-    //also act as state machine to indicate whether to measure or to push the result to the main register
-
-    always @(posedge signal_input) begin
-        //Measurement flag controller, used for resetting flags in various condition
-        //also used for controlling interpolation operation because of the FSM lagging by 1 signal cycle
-        if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) begin 
-            measurement_begin <= 0;
-            measurement_is_done <= 0;
-            counter_ready <= 0;
-        end else if (counter_control_reg[7] == 1 && measurement_is_done == 0) begin
-            measurement_begin <= 1;
-            counter_ready <= 1'b0;
-        end
-        else if (measurement_is_done == 1'b1) begin 
-            measurement_is_done <= 0;
-            measurement_begin <= 0;
-            counter_ready <= 1'b1;
-        end else begin
-            measurement_begin <= 0;
-            counter_ready <= 1'b1;
-        end
-		
-        //FSM driver code
-        if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) begin 
-            measurement_state_machine <= 10'd0;
-        end else if (measurement_begin == 1 && measurement_is_done == 0) begin 
-            if (measurement_state_machine == 16'd998) begin
-                measurement_is_done <= 1'b1;
-                measurement_state_machine <= 10'd0;
-                status[0] <= 1'b1;
-            end else begin 
-                measurement_state_machine <= measurement_state_machine + 1'd1;
-                status[0] <= 1'b0;
-            end
-        end
-        else measurement_state_machine <= measurement_state_machine;
-    end
-
-    always @(posedge reference_clk_main) begin
-        //push to bus-facing register if measurement is done
-        if (rst_i == 1'b1 || counter_reset_internal == 1'b1) measurement_count_internal <= 32'd0;
-        else if (measurement_begin == 1'b1) begin 
-            status[1] <= 1'b1;
-		    if (measurement_is_done == 1'b1) begin
-				measurement_count_internal <= measurement_count_internal;
-				//measurement_count_internal <= 32'd0;
-            end else measurement_count_internal <= measurement_count_internal + 1'b1;
-        end
-        else begin 
-            measurement_count_internal <= 32'd0;
-            status[1] <= 1'b0;
-        end
-	
-        //Interpolation register for the beginning of measurement, at rising edge of measurement_begin
-        if (measurement_begin == 1'b1 && interpolation_in_beginning_done == 1'b0) begin 
-            interpolation_in_beginning_done <= 1'b1;
-            phase_count_intermediate[4] <= phase_count_internal[4];
-            phase_count_intermediate[3] <= phase_count_internal[3];
-            phase_count_intermediate[2] <= phase_count_internal[2];
-            phase_count_intermediate[1] <= phase_count_internal[1];
-            phase_count_intermediate[0] <= phase_count_internal[0];
-        end
-
-        //interplation for end of measurement, at rising edge of measurement_is_done
-        if (measurement_is_done == 1'b1) begin
-            interpolation_in_beginning_done <= 1'b0;
-            phase_count_intermediate[9] <= phase_count_internal[4];
-            phase_count_intermediate[8] <= phase_count_internal[3];
-            phase_count_intermediate[7] <= phase_count_internal[2];
-            phase_count_intermediate[6] <= phase_count_internal[1];
-            phase_count_intermediate[5] <= phase_count_internal[0];
-        end
-
-        //if (counter_pll_1[31] == 1'b1) begin
-			blinker_3 <= ~blinker_3;
-			//counter_pll_1 <= 32'd0;
-		//end else begin
-			//counter_pll_1 <= counter_pll_1 + 32'h2b;
-		//end
-    end
-
-    //internal interpolation measurement begins here, using flip-flop that goes 1 and 0 like waves
-    always @(posedge reference_clk_main) begin
-        phase_count_internal[4] <= ~phase_count_internal[4];
-    end
-
-    always @(posedge reference_clk_interpolate[3]) begin
-        phase_count_internal[3] <= ~phase_count_internal[3];
-    end
-
-    always @(posedge reference_clk_interpolate[2]) begin
-        phase_count_internal[2] <= ~phase_count_internal[2];
-    end
-
-    always @(posedge reference_clk_interpolate[1]) begin
-        phase_count_internal[1] <= ~phase_count_internal[1];
-    end
-
-    always @(posedge reference_clk_interpolate[0]) begin
-        phase_count_internal[0] <= ~phase_count_internal[0];
-    end
 
     //this is the bus-facing interface, responsible for storing data and synchronizing with main control unit
     //it contains the register readily accessible by the control unit via wishbone
@@ -169,6 +57,21 @@ module frequency_counter(
     // - bit 7 : measurement begin (1 for start, 0 for stop) 
     // - bit 6 : measurement is done flag (do not write 1, only write 0 for resetting)
     // - bit 0 : counter reset
+
+    reg measurement_end_buffer_1;
+    reg measurement_end_buffer_2;
+    reg counter_ready_buffer_1;
+    reg counter_ready_buffer_2;
+
+    //buffer block goes here
+    //for synchronization between clock domains
+
+    always @(posedge clk_i) begin
+        measurement_end_buffer_1 measurement_end;
+        measurement_end_buffer_2 <= measurement_end_buffer_1;
+        counter_ready_buffer_1 <= counter_ready;
+        counter_ready_buffer_2 <= counter_ready_buffer_1;
+    end
 
     always @(posedge clk_i) begin
         if (rst_i == 1 || counter_control_reg[0] == 1 || ext_rst_i == 0) begin
@@ -204,7 +107,7 @@ module frequency_counter(
             end
         end
     
-        if (measurement_is_done == 1) begin 
+        if (measurement_end_buffer_2 == 1) begin 
             counter_control_reg[6] <= 1;
             counter_control_reg[7] <= 0;
             measurement_count_reg <= measurement_count_internal;
@@ -234,7 +137,187 @@ module frequency_counter(
             endcase
         end else if (counter_control_reg[0] == 1) counter_control_reg <= 8'd0;
 
-        counter_control_reg[5] <= counter_ready;
+        counter_control_reg[5] <= counter_ready_buffer_2;
+    end
+
+    /*
+    Counter front-end begins here
+    used to interact with the input signal and reference clock
+    */
+
+    reg [31:0] measurement_count_internal;       //front-end measurement counter register, where counting happens and data stored before being pushed to the bus-facing register
+    reg [4:0] phase_count_internal;             //front-end measurement counter register, where counting happens and data stored before being pushed to the bus-facing register
+    reg [9:0] phase_count_intermediate;
+    reg [15:0] measurement_state_machine;        //state machine to indicate whether input signal rising edge is present or not
+    reg measurement_began;                       //state of the measurement state machine from the last clock cycle
+    reg measurement_end;                     //register of whether a measurement has been done or not, useful when waiting for rising edge
+    reg counter_ready;
+    reg [31:0] counter_pll_1;
+
+    reg measurement_begin_buffer_1;
+    reg measurement_begin_buffer_2;
+
+    //buffer blocks here, it simply uses the incoming clock signal to propagates the input
+    always @(posedge signal_input) begin
+        measurement_begin_buffer_1 <= counter_control_reg[7];
+        measurement_begin_buffer_2 <= measurement_begin_buffer_1;
+    end
+
+    always @(posedge signal_input) begin
+        /*
+        //Measurement flag controller, used for resetting flags in various condition
+        //also used for controlling interpolation operation because of the FSM lagging by 1 signal cycle
+        if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) begin 
+            measurement_begin <= 0;
+            measurement_is_done <= 0;
+            counter_ready <= 0;
+        end else if (measurement_begin_buffer_2 == 1 && measurement_is_done == 0) begin
+            measurement_begin <= 1;
+            counter_ready <= 1'b0;
+        end
+        else if (measurement_is_done == 1'b1) begin 
+            measurement_is_done <= 0;
+            measurement_begin <= 0;
+            counter_ready <= 1'b1;
+        end else begin
+            measurement_begin <= 0;
+            counter_ready <= 1'b1;
+        end
+        */
+
+        //new FSM code 
+        case (measurement_state_machine)
+            16'd0 : begin
+                if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) begin 
+                    measurement_state_machine <= 16'd0;
+                    counter_ready <= 1'b0;
+                    measurement_began <= 1'b0;
+                    measurement_end <= 1'b0;
+                end else if (measurement_begin_buffer_2 == 1) begin
+                    measurement_began <= 1'b1;
+                    counter_ready <= 1'b0;
+                    measurement_state_machine <= measurement_state_machine + 1'b1;
+                end else begin
+                    counter_ready <= 1'b1;
+                    measurement_began <= 1'b0;
+                    measurement_end <= 1'b0;
+                    measurement_state_machine <= measurement_state_machine;
+                end
+            end
+            16'd999 : begin
+                if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) begin 
+                    measurement_state_machine <= 16'd0;
+                end else if (measurement_begin_buffer_2 == 1'b0) begin
+                    measurement_state_machine <= 16'd0;
+                end else begin 
+                    measurement_state_machine <= measurement_state_machine;
+                    measurement_end <= 1'b1;
+                end
+            end
+            default : begin 
+                if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) begin 
+                    measurement_state_machine <= 16'd0;
+                end else measurement_state_machine <= measurement_state_machine + 1'b1;
+            end
+        endcase
+    end
+
+    reg interpolation_in_beginning_done;
+    reg interpolation_in_end_done;
+
+    always @(posedge signal_input) begin 
+        //Interpolation register for the beginning of measurement, at rising edge of measurement_begin
+        if (measurement_begin_buffer_2 == 1'b1 && interpolation_in_beginning_done == 1'b0) begin 
+            interpolation_in_beginning_done <= 1'b1;
+            phase_count_intermediate[4] <= phase_count_internal[4];
+            phase_count_intermediate[3] <= phase_count_internal[3];
+            phase_count_intermediate[2] <= phase_count_internal[2];
+            phase_count_intermediate[1] <= phase_count_internal[1];
+            phase_count_intermediate[0] <= phase_count_internal[0];
+        end
+
+        //interplation for end of measurement, at the end of measurement_state_machine
+        else if (measurement_state_machine == 16'd999 & interpolation_in_end_done == 1'b0) begin
+            interpolation_in_end_done <= 1'b1;
+            phase_count_intermediate[9] <= phase_count_internal[4];
+            phase_count_intermediate[8] <= phase_count_internal[3];
+            phase_count_intermediate[7] <= phase_count_internal[2];
+            phase_count_intermediate[6] <= phase_count_internal[1];
+            phase_count_intermediate[5] <= phase_count_internal[0];
+        end
+
+        else if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) begin 
+            phase_count_intermediate[9] <= 1'b0;
+            phase_count_intermediate[8] <= 1'b0;
+            phase_count_intermediate[7] <= 1'b0;
+            phase_count_intermediate[6] <= 1'b0;
+            phase_count_intermediate[5] <= 1'b0;
+            phase_count_intermediate[4] <= 1'b0;
+            phase_count_intermediate[3] <= 1'b0;
+            phase_count_intermediate[2] <= 1'b0;
+            phase_count_intermediate[1] <= 1'b0;
+            phase_count_intermediate[0] <= 1'b0;
+        end
+
+        else begin
+            phase_count_intermediate[9] <= phase_count_intermediate[9];
+            phase_count_intermediate[8] <= phase_count_intermediate[8];
+            phase_count_intermediate[7] <= phase_count_intermediate[7];
+            phase_count_intermediate[6] <= phase_count_intermediate[6];
+            phase_count_intermediate[5] <= phase_count_intermediate[5];
+            phase_count_intermediate[4] <= phase_count_intermediate[4];
+            phase_count_intermediate[3] <= phase_count_intermediate[3];
+            phase_count_intermediate[2] <= phase_count_intermediate[2];
+            phase_count_intermediate[1] <= phase_count_intermediate[1];
+            phase_count_intermediate[0] <= phase_count_intermediate[0];
+        end 
+        //if (counter_pll_1[31] == 1'b1) begin
+			blinker_3 <= ~blinker_3;
+			//counter_pll_1 <= 32'd0;
+		//end else begin
+			//counter_pll_1 <= counter_pll_1 + 32'h2b;
+		//end
+    end
+
+    always @(posedge reference_clk_main) begin
+        //push to bus-facing register if measurement is done
+        if (rst_i == 1'b1 || counter_reset_internal == 1'b1) measurement_count_internal <= 32'd0;
+        else if (measurement_begin == 1'b1) begin 
+            status[1] <= 1'b1;
+		    if (measurement_end == 1'b1) begin
+				measurement_count_internal <= measurement_count_internal;
+            end else measurement_count_internal <= measurement_count_internal + 1'b1;
+        end
+        else begin 
+            measurement_count_internal <= 32'd0;
+            status[1] <= 1'b0;
+        end
+    end
+
+    //internal interpolation measurement begins here, using flip-flop that goes 1 and 0 like waves
+    always @(posedge reference_clk_main) begin
+        if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) phase_count_internal[4] <= 1'b0;
+        else phase_count_internal[4] <= ~phase_count_internal[4];
+    end
+
+    always @(posedge reference_clk_interpolate[3]) begin
+        if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) phase_count_internal[3] <= 1'b0;
+        else phase_count_internal[3] <= ~phase_count_internal[3];
+    end
+
+    always @(posedge reference_clk_interpolate[2]) begin
+        if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) phase_count_internal[2] <= 1'b0;
+        else phase_count_internal[2] <= ~phase_count_internal[2];
+    end
+
+    always @(posedge reference_clk_interpolate[1]) begin
+        if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) phase_count_internal[1] <= 1'b0;
+        else phase_count_internal[1] <= ~phase_count_internal[1];
+    end
+
+    always @(posedge reference_clk_interpolate[0]) begin
+        if (rst_i == 1 || ext_rst_i == 0 || counter_reset_internal == 1) phase_count_internal[0] <= 1'b0;
+        else phase_count_internal[0] <= ~phase_count_internal[0];
     end
 
     assign counter_reset_internal = counter_control_reg[0]; 
